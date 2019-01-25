2994c1e0cf7f1cc2fa4057549299a5d941d43a65
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-25 22:01:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,7 +19,7 @@ def first_element(my_first_element)
  def last_element(my_last_element)
    @taylor_swift[-1]
  end
-def length(length_of_array)
+def length_of_array
   ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"].length
   
 end
