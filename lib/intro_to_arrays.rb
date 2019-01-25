b547a9be58792d12aade98ce52f815a592dfcd2c
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-25 21:49:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,7 +19,7 @@ def first_element(my_first_element)
  def last_element(my_last_element)
    @taylor_swift[-1]
  end
-def first_elemet_with_array_method(first_country)
+def first_element_with_array_method(first_country)
   @south_east_asia.index("first_country")
 end
   
