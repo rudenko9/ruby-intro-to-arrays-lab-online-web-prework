0aaa0dfd7688f1b4a00911d334b961c05410502c
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-25 21:04:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,7 +8,6 @@ def array_with_two_elements
   @my_new_arrays = ["nina", "dasha"]
 end
 
-def my_first_element(firs_element)
-   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-   @taylor_swift[0]
+def my_first_element(firs_element, @taylor_swift)
+   first_elemtn [0]
  end
