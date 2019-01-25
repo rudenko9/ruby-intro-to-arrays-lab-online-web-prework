dfbf73e30b887e0007fa0a9785740217c8617f70
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-25 22:19:52 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,36 @@
+def instantiate_new_array
+  @my_new_arrays = ["nina", "dasha", "mila"]
+  @my_new_arrays = []
+end
+
+def array_with_two_elements
+  @my_new_arrays = ["nina", "dasha", "mila"]
+  @my_new_arrays = ["nina", "dasha"]
+end
+
+def first_element(my_first_element)
+   @taylor_swift[0]
+ end
+ 
+ def third_element(my_third_element)
+   @taylor_swift[2]
+ end
+ 
+ def last_element(my_last_element)
+   @taylor_swift[-1]
+ end
+ 
+ def first_element_with_array_methods(first_country)
+   @south_east_asia.first
+ end
+ 
+ def last_element_with_array_methods(last_country)
+   @south_east_asia.last
+ end
+ 
+def length_of_array(length)
+  ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"].length
+  
+end
+  
+ 
